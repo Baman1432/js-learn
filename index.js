8c6cef8b20cb32f789e77a5cab4d472c4bfba537
Baman1432 <sharmarimanshu48@gmail.com>
2026-02-12 17:30:28 +0000

    variables

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 console.log("rimanshu sharma")
+
+const accountid = "1432"
+let accountname = "Baman marara"
+var accountpassword = "0123"
+ accountcity = "gurdaspur"
+
+//  const accountid = "0147"\\
+  console.log(accountid);
+
+   accountname ="marara baman"
+  accountpassword ="3210"
+  accountcity ="pathankot"
+
+  console.table([accountid , accountname ,accountpassword , accountcity])
+
+const obj = {
+    name: "rimanshu",
+    age: 25
+}
+console.log(obj.name)
+obj.name = "sharma"
+console.log(obj.name)
