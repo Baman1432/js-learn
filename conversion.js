499ee20115478a3bf3f2070f3d5031e349f82b15
Baman1432 <sharmarimanshu48@gmail.com>
2026-02-16 17:08:28 +0000

    conversion of datatypes

---
+++ conversion.js
@@ -0,0 +1,27 @@
+let score = "Baman"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score) 
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//"33" => 33
+// "" => NaN
+// true => 1 false => 0
+
+let IsloggedIn = "Baman"
+
+let booleanIsloggedIn = Boolean(IsloggedIn)
+//console.log(booleanIsloggedIn);
+
+// 0 => false 1 => ture
+// "" => false
+// "Baman" => True
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
