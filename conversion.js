f6fdba71baf9ceaa7f8be75510d3625269a85037
Baman1432 <sharmarimanshu48@gmail.com>
2026-02-16 17:54:31 +0000

    typecasting

--- conversion.js
+++ conversion.js
@@ -1,11 +1,11 @@
-let score = "Baman"
+let score = 0
 
 //console.log(typeof score);
 //console.log(typeof (score));
 
 let valueInNumber = Number(score) 
-//console.log(typeof valueInNumber);
-//console.log(valueInNumber);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
 
 //"33" => 33
 // "" => NaN
@@ -23,5 +23,30 @@ let booleanIsloggedIn = Boolean(IsloggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *******************Operations********************************
+
+let value = 3
+let Negvalue = -value
+// console.log(Negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/3);
+// console.log(2%3);
+// console.log(2**2);
+
+let str1 = "hello"
+let str2 = " Baman"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
