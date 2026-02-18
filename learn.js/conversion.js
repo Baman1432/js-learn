152f707eedc3380f91ec8b2c0e1c1a2e84ccc4e2
Baman1432 <sharmarimanshu48@gmail.com>
2026-02-18 17:33:21 +0000

    change a location

---
+++ learn.js/conversion.js
@@ -0,0 +1,65 @@
+let score = 0
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score) 
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33" => 33
+// "" => NaN
+// true => 1 false => 0
+
+let IsloggedIn = "Baman"
+
+let booleanIsloggedIn = Boolean(IsloggedIn)
+//console.log(booleanIsloggedIn);
+
+// 0 => false 1 => ture
+// "" => false
+// "Baman" => True
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *******************Operations********************************
+
+let value = 3
+let Negvalue = -value
+// console.log(Negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/3);
+// console.log(2%3);
+// console.log(2**2);
+
+let str1 = "hello"
+let str2 = " Baman"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(2 > 0);
+// console.log(2 < 0);
+// console.log(2 == 0);
+// console.log(2 >= 0);
+// console.log(2 <= 0);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+console.log(undefined === 0);
+console.log(undefined <= 0);
+console.log(undefined > 0);
