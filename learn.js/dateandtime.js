fefaa2cabdc2edd4969a4b7a72924eef78aad67b
Baman1432 <sharmarimanshu48@gmail.com>
2026-03-01 17:12:08 +0000

    date and time

---
+++ learn.js/dateandtime.js
@@ -0,0 +1,39 @@
+// Dates
+
+
+let mydate = new Date()
+
+// console.log(mydate);
+// console.log(mydate.toString()); 
+// console.log(mydate.toLocaleString()); 
+// console.log(mydate.toLocaleDateString()); 
+// console.log(mydate.toISOString()); 
+// console.log(mydate.toJSON()); 
+// console.log(mydate.toLocaleTimeString()); 
+
+// let mycreateddate = new Date(2026, 0, 1)
+// console.log(mycreateddate);
+// console.log(mycreateddate.toDateString());
+
+// let mycreateddate = new Date(2026, 0, 1, 9, 5)
+// console.log(mycreateddate.toLocaleString());
+
+// let mycreateddate = new Date(2026-01-01)
+// console.log(mycreateddate.toLocaleString());
+
+// let mycreateddate = new Date("01-20-2026")
+// console.log(mycreateddate.toLocaleString());
+
+// let myTimestamp = Date.now()
+// let mycreateddate = new Date("01-20-2026")
+// console.log(myTimestamp);
+// console.log(mycreateddate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newdate = new Date()
+// console.log(newdate);
+// console.log(newdate.getMonth() + 1);
+// console.log(newdate.getDay() + 1);
+// console.log(newdate.getFullYear());
+
+
